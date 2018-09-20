66cb58818d385b4ddda856c327b4b5b8e2c67d98
Leonardo_zyh <956731563@qq.com>
2018-09-20 15:06:21 +0800

    点击激活歌曲项

--- src/js/new-song.js
+++ src/js/new-song.js
@@ -2,7 +2,7 @@
     let view = {
         el:'#newSong',
         template:`
-        新建歌曲
+        <span>新建歌曲<span>
         `,
         render(data){
             $(this.el).html(this.template)
@@ -18,9 +18,15 @@
             window.eventHub.on('upload',(data)=>{                            
                 this.active()
             })
+            window.eventHub.on('select',(data)=>{
+                this.deactive()
+            })
         },
         active(){
             $(this.view.el).addClass('active')
+        },
+        deactive(){
+            $(this.view.el).removeClass('active')
         }
     }
     controller.init(view,model)
--- src/js/songList-container.js
+++ src/js/songList-container.js
@@ -10,7 +10,7 @@
             let $el = $(this.el)
             $el.html(this.template)
             let { songs } = data
-            let liList = songs.map((song) => $('<li></li>').text(song.name))
+            let liList = songs.map((song) => $('<li></li>').text(song.name).attr('data-song-id',song.id))
             $el.find('ul').empty()
             liList.map((domLi) => {
                 $el.find('ul').append(domLi)
@@ -18,6 +18,11 @@
         },
         clearActive() {
             $(this.el).find('.active').removeClass('active')
+        },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active')
         }
     }
     let model = {
@@ -39,7 +44,24 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
+            this.bindEvents()
+            this.bindEventHub()
+            this.getAllSongs()
+        },
+        getAllSongs(){
+            return this.model.find().then(()=>{
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id:songId})
+            })
 
+        },
+        bindEventHub(){
             window.eventHub.on('upload', () => {
                 this.view.clearActive()
             })
@@ -47,9 +69,6 @@
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
                 console.log(this.model.data);
-            })
-            this.model.find().then(()=>{
-                this.view.render(this.model.data)
             })
         }
     }
